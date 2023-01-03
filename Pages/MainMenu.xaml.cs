3140801c68f1f52c3df5059a2679ce5ddcedee58
Catalin Maeschi <catalin.maeschi23@gmail.com>
2023-01-03 18:57:15 +0200

    MainMenu

---
+++ Pages/MainMenu.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace Bank_Management_System.Pages
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AccountsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TransactionsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
