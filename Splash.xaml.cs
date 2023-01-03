9385dd74557b7fc9322fe5df4fab0e935353ddf5
Catalin Maeschi <catalin.maeschi23@gmail.com>
2023-01-03 15:05:17 +0200

    Add project files.

---
+++ Splash.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Bank_Management_System
+{
+    public partial class Splash : Window
+    {
+        public Splash()
+        {
+            InitializeComponent();
+        }
+    }
+}
