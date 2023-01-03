76dd3b0fad6e51426e2ecc5f43e05a443bacf89d
Catalin Maeschi <catalin.maeschi23@gmail.com>
2023-01-03 17:35:07 +0200

    Login Page

---
+++ Login.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace Bank_Management_System
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
